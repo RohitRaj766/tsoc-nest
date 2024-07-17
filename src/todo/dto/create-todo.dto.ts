f2b0212c2d78c58dce83efae7376c8cef371f628
RohitRaj766 <rohitraj2k04@gmail.com>
2024-07-17 10:30:52 +0530

    setting up remote postgress

--- src/todo/dto/create-todo.dto.ts
+++ src/todo/dto/create-todo.dto.ts
@@ -1,6 +1,6 @@
 /* eslint-disable prettier/prettier */
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
-import { IsNotEmpty, IsString, IsEmail } from 'class-validator';
+import { IsNotEmpty, IsString, } from 'class-validator';
 
 export class CreateTodoDto {
   @ApiProperty()
